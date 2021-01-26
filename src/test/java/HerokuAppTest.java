9a370b9c752b693e10c343534f70397a3222a3ef
alla_trinda <alla@mailinator.com>
2021-01-26 20:35:59 +0300

    Initial commit

---
+++ src/test/java/HerokuAppTest.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class HerokuAppTest {
+
+    @Test
+    public void siteShouldBeOpened() {
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");//хром хранится вот тут
+        WebDriver browser = new ChromeDriver();
+        browser.get("http://the-internet.herokuapp.com/");
+        browser.quit();
+    }
+}
